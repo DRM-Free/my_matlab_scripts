d61de6582aee7656445fb2fbeec33823fbeee2d4
Anaël Leinert <leinert.anael@gmail.com>
2018-04-26 18:30:47 +0200

    Rois recovery from sData properly done and processing of all dice coefficients as well

--- ROIs gen/diceCoefficient_distrib_all_rois.m
+++ ROIs gen/diceCoefficient_distrib_all_rois.m
@@ -1,16 +1,32 @@
-function [dice]=diceCoefficient_distrib_all_rois()
-try
-   load('all_vols');
-catch
-all_vols=get_all_vols('DATA/Lung-Multidelineation-05_CT.CTscan.mat');
-end
+function [all_dice]=diceCoefficient_distrib_all_rois()
+
+% all_data=struct;
+% all_data.name='Lung-Multidelineation-05_CT.CTscan.mat';
+% cur_data=1;
+
+all_data=dir('DATA/*.mat')
+for cur_data=1:numel(all_data)
+all_vols=get_all_vols(strcat('DATA/',all_data(cur_data).name));
 pairs=gen_pairs_1D(size(all_vols,1));
-for pair=1:numel(pairs(1))
+if numel(pairs(:,1))>45
+    %It appears that an abnormally big number of ROIs were introduced
+    %somehow. This needs further investigation
+    % example : Lung-Multidelineation-08_CT.CTscan.mat
+end
+for pair=1:numel(pairs(:,1))
     pair_Indices=pairs(pair,:);
     try
         dice(end+1)=dice_coeff_from_vol(all_vols{pair_Indices(1)},all_vols{pair_Indices(2)});
     catch
         dice=dice_coeff_from_vol(all_vols{pair_Indices(1)},all_vols{pair_Indices(2)});
     end
+end
+try
+    all_dice(cur_data)={dice};
+catch
+    all_dice={dice};
+end
+clear dice;
+fprintf('%i of %i data files were processed\n',cur_data,numel(all_data));
 end
 end
--- ROIs gen/get_all_vols.m
+++ ROIs gen/get_all_vols.m
@@ -7,8 +7,8 @@ for roiNum=1:numel(rois) %First iterate over all rois in order to determine the 
     
     %Removing unused rois
     roiName=roiName.name;
-    visPos = strfind(roiName,'vis');
-    autoPos = strfind(roiName,'auto');
+    visPos = strfind(roiName,'1vis');
+    autoPos = strfind(roiName,'1auto');
     
     if ~(isequal(autoPos,[])&isequal(visPos,[]))
         process=true;
@@ -16,7 +16,6 @@ for roiNum=1:numel(rois) %First iterate over all rois in order to determine the 
         process=false;
     end
     if process
-        %         [volObj,roiObj] = getROI(sData,roiNum,'2box');
         [~,roiObj] = getROI(sData,roiNum,'box'); %Getting minimal cube inlcuding the tumor
         xmin=roiObj.spatialRef.XIntrinsicLimits(1); xmax=roiObj.spatialRef.XIntrinsicLimits(2);
         ymin=roiObj.spatialRef.YIntrinsicLimits(1);ymax=roiObj.spatialRef.YIntrinsicLimits(2);
@@ -28,18 +27,25 @@ for roiNum=1:numel(rois) %First iterate over all rois in order to determine the 
             min_global=[min(min_global(1),xmin),min(min_global(2),ymin),min(min_global(3),zmin)];
             max_global=[max(max_global(1),xmax),max(max_global(2),ymax),max(max_global(3),zmax)];
         end
+        try
+            [tr_ref_x,tr_ref_y,tr_ref_z]=min([tr_ref_x,tr_ref_y,tr_ref_z],worldToIntrinsic(roiObj.spatialRef,roiObj.spatialRef.XIntrinsicLimits(1),roiObj.spatialRef.YIntrinsicLimits(1),roiObj.spatialRef.ZIntrinsicLimits(1)));
+        catch
+            [tr_ref_x,tr_ref_y,tr_ref_z]=worldToIntrinsic(roiObj.spatialRef,roiObj.spatialRef.XIntrinsicLimits(1),roiObj.spatialRef.YIntrinsicLimits(1),roiObj.spatialRef.ZIntrinsicLimits(1));
+            
+        end
     end
 end
 
-        [~,roiObj] = getROI(sData,roiNum,'full'); %here find the translate to apply to all boxes
-        %If there was a step to monitor closely it should be this one.
-        [translate_x,translate_y,translate_z]=worldToIntrinsic(roiObj.spatialRef,roiObj.spatialRef.XIntrinsicLimits(1),roiObj.spatialRef.YIntrinsicLimits(1),roiObj.spatialRef.ZIntrinsicLimits(1));
+[~,roiObj] = getROI(sData,roiNum,'full'); %here find the translate to apply to all boxes
+%If there was a step to monitor closely it should be this one.
+[translate_x,translate_y,translate_z]=worldToIntrinsic(roiObj.spatialRef,roiObj.spatialRef.XIntrinsicLimits(1),roiObj.spatialRef.YIntrinsicLimits(1),roiObj.spatialRef.ZIntrinsicLimits(1));
+translate=[translate_y,translate_x,translate_z]-[tr_ref_y,tr_ref_x,tr_ref_z];
 
 %New borders to apply to all ROIs
 borderCoords=[min_global(1),max_global(1);min_global(2),max_global(2);min_global(3),max_global(3)];
-borderCoords(1,:)=borderCoords(1,:)+translate_x;
-borderCoords(2,:)=borderCoords(2,:)+translate_y;
-borderCoords(3,:)=borderCoords(3,:)+translate_z;
+borderCoords(1,:)=borderCoords(1,:)+translate(1);
+borderCoords(2,:)=borderCoords(2,:)+translate(2);
+borderCoords(3,:)=borderCoords(3,:)+translate(3);
 
 for roiNum=1:numel(rois) %Now resize ROIs to the common sizes and return all of them
     %We still need to translate referentials for proper registration
@@ -47,8 +53,8 @@ for roiNum=1:numel(rois) %Now resize ROIs to the common sizes and return all of 
     
     %Removing unused rois
     roiName=roiName.name;
-    visPos = strfind(roiName,'vis');
-    autoPos = strfind(roiName,'auto');
+    visPos = strfind(roiName,'1vis');
+    autoPos = strfind(roiName,'1auto');
     
     if ~(isequal(autoPos,[])&isequal(visPos,[]))
         process=true;
@@ -57,10 +63,12 @@ for roiNum=1:numel(rois) %Now resize ROIs to the common sizes and return all of 
     end
     if process
         [volObj,roiObj] = getROI(sData,roiNum,'full'); %Getting minimal cube inlcuding the tumor
-
-        % COMPUTING THE BOUNDING BOX
-        [~,roi,newSpatialRef] = compute_custom_box(volObj.data,roiObj.data,roiObj.spatialRef,borderCoords);
         
+        % COMPUTING THE BOUNDING BOX
+        try
+            [~,roi,newSpatialRef] = compute_custom_box(volObj.data,roiObj.data,roiObj.spatialRef,borderCoords);
+        catch
+        end
         % ARRANGE OUTPUT
         newROI_Obj = struct;
         newROI_Obj.data = roi; newROI_Obj.spatialRef = newSpatialRef;
@@ -72,7 +80,6 @@ for roiNum=1:numel(rois) %Now resize ROIs to the common sizes and return all of 
     end
 end
 
-
 clear sData;
-save('all_vols','all_vols');
+% save('all_vols','all_vols');
 end
