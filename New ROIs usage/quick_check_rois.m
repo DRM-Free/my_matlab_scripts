223cd1dd89bb128ba7172a3e4317576de68183ac
Anaël Leinert <leinert.anael@gmail.com>
2018-05-15 15:46:18 +0200

    Minor modifications

--- New ROIs usage/quick_check_rois.m
+++ New ROIs usage/quick_check_rois.m
@@ -1,4 +1,4 @@
-function [original_roi,expand_roi,shrink_roi]=quick_check_rois(sData)
+function [original_roi,expand_roi,expand_roi2,shrink_roi,shrink_roi2]=quick_check_rois(sData)
 %This function serves one purpose : get quick access to newly generated
 %rois in order to visually compare them and check everything is in order
 [~,roiObj] = getROI(sData,4,'2box');
@@ -7,8 +7,12 @@ original_roi=roiObj_regular.data;
 % world_extent=[sData{1,2}.scan.volume.spatialRef.PixelExtentInWorldX,sData{1,2}.scan.volume.spatialRef.PixelExtentInWorldY,sData{1, 2}.scan.volume.spatialRef.PixelExtentInWorldZ];
 roi=sData{1,2}.scan.contour(4).simple_roi_gen;
 roiex=roi.expand_dice_range_07_to_08{1};
+roiex2=roi.expand_dice_range_07_to_08{2};
 roisr=roi.shrink_dice_range_07_to_08{1};
+roisr2=roi.shrink_dice_range_07_to_08{2};
 sz=size(original_roi);
 expand_roi=matrix_from_indices(sz,roiex);
+expand_roi2=matrix_from_indices(sz,roiex2);
 shrink_roi=matrix_from_indices(sz,roisr);
+shrink_roi2=matrix_from_indices(sz,roisr2);
 end
