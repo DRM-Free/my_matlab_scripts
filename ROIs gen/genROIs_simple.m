83331cabc426c6cfa4ecd1cf5fd669775271be52
Anaël Leinert <leinert.anael@gmail.com>
2018-06-20 10:29:37 +0200

    Update genROIs_simple.m

--- ROIs gen/genROIs_simple.m
+++ ROIs gen/genROIs_simple.m
@@ -1,3 +1,17 @@
+%MIT License
+
+%Copyright (c) 2018 Anaël Leinert
+
+%Permission is hereby granted, free of charge, to any person obtaining a copy
+%of this software and associated documentation files (the "Software"), to deal
+%in the Software without restriction, including without limitation the rights
+%to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+%copies of the Software, and to permit persons to whom the Software is
+%furnished to do so, subject to the following conditions:
+
+%The above copyright notice and this permission notice shall be included in all
+%copies or substantial portions of the Software
+
 function [all_simple_rois,shrink_thresholds,expand_thresholds]= genROIs_simple(roiObj,world_extent,range,nIter)
 %range is the size of the area to be considered around each point for growth or shrink.
 %threshold represents how easy it will be to grow or shrink. 0 will always
