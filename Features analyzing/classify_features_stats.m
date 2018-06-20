7f58732244fbdc3067fc47ebc368768897d8c618
Anaël Leinert <leinert.anael@gmail.com>
2018-06-20 11:07:49 +0200

    Update classify_features_stats.m

--- Features analyzing/classify_features_stats.m
+++ Features analyzing/classify_features_stats.m
@@ -1,3 +1,17 @@
+%MIT License
+
+%Copyright (c) 2018 Anaël Leinert
+
+%Permission is hereby granted, free of charge, to any person obtaining a copy
+%of this software and associated documentation files (the "Software"), to deal
+%in the Software without restriction, including without limitation the rights
+%to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+%copies of the Software, and to permit persons to whom the Software is
+%furnished to do so, subject to the following conditions:
+
+%The above copyright notice and this permission notice shall be included in all
+%copies or substantial portions of the Software
+
 function [kept_morph,kept_stat]=classify_features_stats(all_test_results)
 %Extract classification elements
 mean_scores=all_test_results.mean_scores;
