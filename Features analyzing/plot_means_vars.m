60603e3fc43bd2fe9a738854ce866bf7531b2283
Anaël Leinert <leinert.anael@gmail.com>
2018-06-20 11:10:47 +0200

    Update plot_means_vars.m

--- Features analyzing/plot_means_vars.m
+++ Features analyzing/plot_means_vars.m
@@ -1,3 +1,17 @@
+%MIT License
+
+%Copyright (c) 2018 Anaël Leinert
+
+%Permission is hereby granted, free of charge, to any person obtaining a copy
+%of this software and associated documentation files (the "Software"), to deal
+%in the Software without restriction, including without limitation the rights
+%to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+%copies of the Software, and to permit persons to whom the Software is
+%furnished to do so, subject to the following conditions:
+
+%The above copyright notice and this permission notice shall be included in all
+%copies or substantial portions of the Software
+
 function [mean_relative_variation,var_relative_variation]=plot_means_vars(features_mean,features_mean_original,features_var,features_var_original,feature)
 original_mean=get_inclusive_field(features_mean_original.FEATURES,feature);
 original_var=get_inclusive_field(features_var_original.FEATURES,feature);
